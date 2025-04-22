41bbc27c591ab11da7f247feff54d99ce6705f3d
alecps <alec@cLabs.co>
2025-04-22 16:48:09 -0400

    patch attempt 1

--- algebra-core/src/curves/models/short_weierstrass_jacobian.rs
+++ algebra-core/src/curves/models/short_weierstrass_jacobian.rs
@@ -354,7 +354,7 @@ impl<P: Parameters> ProjectiveCurve for GroupProjective<P> {
 
     #[inline]
     fn batch_normalization(v: &mut [Self]) {
-        // Montgomery’s Trick and Fast Implementation of Masked AES
+        // Montgomery's Trick and Fast Implementation of Masked AES
         // Genelle, Prouff and Quisquater
         // Section 3.2
 
@@ -420,7 +420,9 @@ impl<P: Parameters> ProjectiveCurve for GroupProjective<P> {
             let d = ((self.x + &b).square() - &a - &c).double();
 
             // E = 3*A
-            let e = a + a.double_in_place();
+            let mut a_double = a.clone();
+            a_double.double_in_place();
+            let e = a + a_double;
 
             // F = E^2
             let f = e.square();
@@ -433,7 +435,12 @@ impl<P: Parameters> ProjectiveCurve for GroupProjective<P> {
             self.x = f - &d - &d;
 
             // Y3 = E*(D-X3)-8*C
-            self.y = (d - &self.x) * &e - c.double_in_place().double_in_place().double_in_place();
+            let d_minus_x = d - &self.x;
+            let mut c_doubled = c.clone();
+            c_doubled.double_in_place();
+            c_doubled.double_in_place();
+            c_doubled.double_in_place();
+            self.y = d_minus_x * &e - c_doubled;
             self
         } else {
             // http://www.hyperelliptic.org/EFD/g1p/auto-shortw-jacobian-0.html#doubling-dbl-2009-l
@@ -462,7 +469,12 @@ impl<P: Parameters> ProjectiveCurve for GroupProjective<P> {
             self.x = t;
             // Y3 = M*(S-T)-8*YYYY
             let old_y = self.y;
-            self.y = m * &(s - &t) - yyyy.double_in_place().double_in_place().double_in_place();
+            let s_minus_t = s - &t;
+            let mut yyyy_doubled = yyyy.clone();
+            yyyy_doubled.double_in_place();
+            yyyy_doubled.double_in_place(); 
+            yyyy_doubled.double_in_place();
+            self.y = m * &s_minus_t - yyyy_doubled;
             // Z3 = (Y1+Z1)^2-YY-ZZ
             self.z = (old_y + &self.z).square() - &yy - &zz;
             self
@@ -623,7 +635,10 @@ impl<'a, P: Parameters> AddAssign<&'a Self> for GroupProjective<P> {
             self.x = r.square() - &j - &(v.double());
 
             // Y3 = r*(V - X3) - 2*S1*J
-            self.y = r * &(v - &self.x) - (s1 * &j).double_in_place();
+            let v_minus_x = v - &self.x;
+            let mut s1j = s1 * &j;
+            s1j.double_in_place();
+            self.y = r * &v_minus_x - s1j;
 
             // Z3 = ((Z1+Z2)^2 - Z1Z1 - Z2Z2)*H
             self.z = ((self.z + &other.z).square() - &z1z1 - &z2z2) * &h;
